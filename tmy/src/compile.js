af2da5c416b92facd16489b7958fd8dae7e5d99c
Tommy <spielhoelle@posteo.net>
2024-09-02 12:59:27 +0900

    fix: links, overflow, flophub

--- tmy/src/compile.js
+++ tmy/src/compile.js
@@ -11,12 +11,12 @@ const items = [{
 	filename: 'keylogger.html',
 	headline: 'Analyse keylogger results',
 	description: 'Capturing keystrokes over several weeks, keep the process in multiple environments active, format the data and elaborating on the results. This gave me insights in daily activity, mistakes, 10-finger typing behavior and more.',
-	content: fs.readFileSync(appDir + 'app/notebooks/keylogger.html', 'utf8')
+	content: fs.readFileSync(appDir + 'notebooks/keylogger.html', 'utf8')
 }, {
 	filename: 'synthetic-data-generation.html',
 	headline: 'Synthetic data generation on a large scale',
 	description: 'From a predefined set of data, generate synthetic data for artificial LLM dataset. This can be adapted to be based on a certain base-truth data corpus. Interesting was the shuffling of random variables in the data creation, parallelism and performance when creating large datasets.',
-	content: fs.readFileSync(appDir + 'app/notebooks/synthetic-data-generation.html', 'utf8')
+	content: fs.readFileSync(appDir + 'notebooks/synthetic-data-generation.html', 'utf8')
 }]
 
 const template = Handlebars.compile(source);
